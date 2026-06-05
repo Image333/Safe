2dfc32c898419baf04e0b99d5a6021f11e673ed0
lamouchedu94 <lamouchedu54@gmail.com>
2026-06-05 15:26:20 +0200

    [ADD] Backend - routes - Get user by email route

--- v0/api/routes/users.go
+++ v0/api/routes/users.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -17,9 +18,22 @@ type CreateUserRequest struct {
 	RoleID    int    `json:"role_id"`
 }
 
+type UserResponse struct {
+	UserID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	Firstname        string `json:"firstname"`
+	Email            string `json:"email"`
+	RegistrationDate string `json:"registration_date"`
+	RoleID           *int   `json:"role_id"`
+	ConfigID         *int   `json:"config_id"`
+}
+
 // RegisterUserRoutes prend le routeur ET la BDD en paramètre
 func RegisterUserRoutes(router fiber.Router, db *sql.DB) {
-	// On crée un handler inline (closure) pour avoir accès à la variable 'db'
+
+	//
+	// Create User
+	//
 	router.Post("/users", func(c *fiber.Ctx) error {
 		var req CreateUserRequest
 
@@ -72,5 +86,53 @@ func RegisterUserRoutes(router fiber.Router, db *sql.DB) {
 			"user_id": newID,
 		})
 
+	})
+
+	//
+	// Get user by email
+	//
+	router.Get("/users/:email", func(c *fiber.Ctx) error {
+		// get email from params
+		emailParam := c.Params("email")
+		if emailParam == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Email requis dans l'URL",
+			})
+		}
+
+		query := `SELECT user_id, name, firstname, email, registration_date, role_id, config_id
+		          FROM users
+		          WHERE email = ?`
+
+		var user UserResponse
+
+		// 3. Exécuter et scanner la ligne
+		// QueryRow s'attend à trouver exactement 1 ligne
+		err := db.QueryRow(query, emailParam).Scan(
+			&user.UserID,
+			&user.Name,
+			&user.Firstname,
+			&user.Email,
+			&user.RegistrationDate,
+			&user.RoleID,
+			&user.ConfigID,
+		)
+
+		if err != nil {
+			// Si l'utilisateur n'existe pas en BDD
+			if errors.Is(err, sql.ErrNoRows) {
+				return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+					"error": "Utilisateur non trouvé",
+				})
+			}
+			// Pour toute autre erreur technique SQL
+			log.Printf("Erreur SQL SELECT: %v", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Erreur lors de la récupération de l'utilisateur",
+			})
+		}
+
+		// 4. Renvoyer les infos de l'utilisateur trouvé
+		return c.Status(fiber.StatusOK).JSON(user)
 	})
 }
